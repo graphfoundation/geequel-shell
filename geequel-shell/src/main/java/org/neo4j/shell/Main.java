73885d7c9d125059dea95610273d24d669a4ec08
Brad Nussbaum <brad@graphfoundation.org>
2021-02-24 14:09:53 -0500

    GEEQUEL-24 - Build config should be in a package that is not ignored

--- geequel-shell/src/main/java/org/neo4j/shell/Main.java
+++ geequel-shell/src/main/java/org/neo4j/shell/Main.java
@@ -22,7 +22,7 @@ package org.neo4j.shell;
 import jline.console.ConsoleReader;
 
 import org.neo4j.driver.v1.exceptions.AuthenticationException;
-import org.neo4j.shell.build.Build;
+import org.neo4j.shell.config.Build;
 import org.neo4j.shell.cli.CliArgHelper;
 import org.neo4j.shell.cli.CliArgs;
 import org.neo4j.shell.commands.CommandHelper;
